06fcde5cf3334d15f3709394b431b77af8e2f56d
Jorge Matricali <jorgematricali@gmail.com>
2023-11-15 21:31:59 -0300

    First draft

--- handler.py
+++ handler.py
@@ -3,42 +3,26 @@ import logging
 import os
 
 import boto3
+import psycopg2
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 QUEUE_URL = os.getenv('QUEUE_URL')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
 SQS = boto3.client('sqs')
-
-
-def producer(event, context):
-    status_code = 200
-    message = ''
-
-    if not event.get('body'):
-        return {'statusCode': 400, 'body': json.dumps({'message': 'No body was found'})}
-
-    try:
-        message_attrs = {
-            'AttributeName': {'StringValue': 'AttributeValue', 'DataType': 'String'}
-        }
-        SQS.send_message(
-            QueueUrl=QUEUE_URL,
-            MessageBody=event['body'],
-            MessageAttributes=message_attrs,
-        )
-        message = 'Message accepted!'
-    except Exception as e:
-        logger.exception('Sending message to SQS queue failed!')
-        message = str(e)
-        status_code = 500
-
-    return {'statusCode': status_code, 'body': json.dumps({'message': message})}
+S3 = boto3.resource('s3')
 
 
 def consumer(event, context):
     for record in event['Records']:
         logger.info(f'Message body: {record["body"]}')
+        s3payload = json.loads(record['Body'])
+
+        content_object = s3.Object(BUCKET_NAME, s3payload['baseKey']);
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+
         logger.info(
-            f'Message attribute: {record["messageAttributes"]["AttributeName"]["stringValue"]}'
+            json_content
         )
