d9b6ffbd600d3dc370ceeaabb9ead230fbaec999
alelb22 <alelb22@gmail.com>
2023-11-16 03:25:22 -0300

    feat: sqs and lambda trigger

---
+++ lambda/SampleName/main.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+
+def handler(event, context):
+  for record in event['Records']:
+    print("test")
+    payload = record["body"]
+    print(str(payload))
+  
+  print("Delay al vicio")
+
+
+
